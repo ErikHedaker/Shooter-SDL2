34fced266d9660e0b6720ea131ec0b7a043efd9c
ErikHedaker <erik@hedaker.se>
2016-02-22 22:53:24 +0100

    Moved files.

--- Shooter-SDL2/main.cpp
+++ Shooter-SDL2/main.cpp
@@ -5,12 +5,12 @@
 
 int main( int argc, char* args[] )
 {
-    const std::string pathResources = "C:\\Users\\Erik\\Documents\\Visual Studio 2015\\Projects\\Dungeoncrawler\\Dungeoncrawler\\Resources\\";
+    const std::string pathResources = "C:\\Users\\Erik\\Documents\\Visual Studio 2015\\Projects\\Shooter-SDL2\\Shooter-SDL2\\Resources\\";
     Game game( "Shooter-SDL2", { 640, 480 } );
 
     game.EntityAdd( pathResources + "white.bmp", { 100, 100 }, { 100, 100 }, Attributes::Renderable );
-    game.EntityAdd( pathResources + "grass.bmp", { 100, 100 }, { 200, 200 }, Attributes::Renderable );
-    game.EntityAdd( pathResources + "white.bmp", { 100, 100 }, { 300, 100 }, Attributes::Renderable );
+    game.EntityAdd( pathResources + "water.bmp", { 100, 100 }, { 200, 200 }, Attributes::Renderable );
+    game.EntityAdd( pathResources + "grass.bmp", { 100, 100 }, { 300, 100 }, Attributes::Renderable );
 
     game.Loop( );
 
